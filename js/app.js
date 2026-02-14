e20df51d9ff862e734bc543a5dd416a903edb9b5
Dominic juma <jumadomi518@gmail.com>
2026-02-14 17:52:52 +0300

    c

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ show.onclick = () => {
 
 // SERVICE WORKER
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("./sw.js")
+  navigator.serviceWorker.register("/sw.js")
     .then(() => console.log("SW registered"))
     .catch(err => console.log("SW registration failed:", err));
 }
